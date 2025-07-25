d74ea4d5daa905da217135099f616535797a3f57
sh4aadman <sh4adman@gmail.com>
2025-07-25 17:32:56 +0600

    index created

---
+++ multi-step-form-main/tailwind.config.js
@@ -0,0 +1,8 @@
+    // tailwind.config.js
+    module.exports = {
+      content: [], // Add paths to your HTML, JS, or other files where you use Tailwind classes
+      theme: {
+        extend: {},
+      },
+      plugins: [],
+    };
