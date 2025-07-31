0eda44272a20f69828a216cf470dceae057affe9
sh4aadman <sh4adman@gmail.com>
2025-07-31 17:52:23 +0600

    mobile-view fixed

--- multi-step-form-main/scripts/observer.js
+++ multi-step-form-main/scripts/observer.js
@@ -1,8 +1,26 @@
 import currentForm from "./currentForm.js";
 
 const observer = new MutationObserver((entries) => {
-  console.log(entries);
-  if (entries.length === 2 && entries[1].type === "attributes" && entries[1].attributeName === "class") {
+  // console.log(entries);
+  if (
+    entries.length === 2 &&
+    entries[1].type === "attributes" &&
+    entries[1].attributeName === "class"
+  ) {
+    const formSection = entries[1].target.children[0];
+    const formData = new FormData(formSection);
+
+    if (entries[1].target.id === "1") {
+      const [name, email, phoneNumber] = formData.values();
+      console.log(name, email, phoneNumber);
+      const userDetails = { name, email, phoneNumber };
+      console.log(userDetails);
+    }
+    if (entries[1].target.id === "2") {
+      console.log("hello");
+      const [plan] = formData.values();
+      console.log(plan);
+    }
     currentForm();
   }
 });
